9586abf3349562fa5177085daf31b72e3d73712b
unknown <danielose266@gmail.com>
2023-02-22 23:09:23 +0100

    started work

---
+++ src/pages/blog.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import React from 'react'
+
+export const Blog = () => {
+    return (
+        <>
+            <Head>
+                
+            </Head>
+        </>
+    )
+}
