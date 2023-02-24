df55ad6d5c7f8753cb97df3fce5e4ff783846814
unknown <danielose266@gmail.com>
2023-02-24 10:20:13 +0100

    fadein only when in view fix

--- src/site/animationWithFramer/fadeInView.tsx
+++ src/site/animationWithFramer/fadeInView.tsx
@@ -1,14 +1,27 @@
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { GlobalTypes } from "../globals";
 
-function FadeInView({ className, children, delay = 0.5 } : GlobalTypes) {
+function FadeInView({ className, children, delay = 0.5 }: GlobalTypes) {
+    const [ref, inView] = useInView({
+        threshold: 0.1, // Trigger animation when component is 10% visible
+        triggerOnce: true, // Only trigger animation once
+    });
+
     const fadeInVariants = {
         hidden: { opacity: 0 },
-        visible: { opacity: 1, transition: {delay} },
+        visible: { opacity: 1 },
     };
 
     return (
-        <motion.div className={`${className} transition-all duration-300`} initial="hidden" animate="visible" variants={fadeInVariants}>
+        <motion.div
+            ref={ref}
+            className={`${className} transition-all duration-300`}
+            initial="hidden"
+            animate={inView ? "visible" : "hidden"}
+            variants={fadeInVariants}
+            transition={{ delay }}
+        >
             {children}
         </motion.div>
     );
