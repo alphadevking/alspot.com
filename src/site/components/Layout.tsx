241fe00858e5c16b62deace45dd92b6a65e726e6
alphadevking <danielose266@gmail.com>
2023-07-16 02:18:32 +0100

    fixed script issue

--- src/site/components/Layout.tsx
+++ src/site/components/Layout.tsx
@@ -1,52 +1,70 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Footer } from './Footer'
 import { GlobalTypes } from '../globals/';
 import { AppWrapper } from './AppWrapper';
 import Navbar from './Navigation/Navbar';
 
-const Layout:React.FC<GlobalTypes> = ({ navbar, children, footer}) => (
+const Layout: React.FC<GlobalTypes> = ({ navbar, children, footer }) => {
 
-    <main className='min-h-screen'>
+    useEffect(() => {
+        const jivositeScript = document.createElement('script');
+        jivositeScript.src = "//code.jivosite.com/widget/OLF8iQTisE";
+        jivositeScript.async = true;
+        document.body.appendChild(jivositeScript);
 
-        <AppWrapper>
+        const gtranslateScript = document.createElement('script');
+        gtranslateScript.src = "https://cdn.gtranslate.net/widgets/latest/float.js";
+        gtranslateScript.defer = true;
+        document.body.appendChild(gtranslateScript);
 
-            <span className='flex fixed z-[999] right-0 top-2/3 shadow-xl bg-white/10 backdrop-blur-[3px] hover:pr-5 rounded-l-3xl p-2 align-middle duration-300 hover:bg-white/20 text-xl animate-bounce'>
-                {/* <ThemeSwitch /> */}
-            </span>
+        return () => { // clean up the scripts on component unmount
+            document.body.removeChild(jivositeScript);
+            document.body.removeChild(gtranslateScript);
+        };
+    }, []); // empty dependency array so the effect only runs once
 
-            {
-                navbar ? 
-                    <Navbar/>
-                    :
-                    null
-            }
+    return (
+        <main className='min-h-screen'>
 
-            <div className='px-5 md:px-32'>
+            <AppWrapper>
 
-                {children}
-                
-                <div className="gtranslate_wrapper"></div>
-                
-            </div>
+                <span className='flex fixed z-[999] right-0 top-2/3 shadow-xl bg-white/10 backdrop-blur-[3px] hover:pr-5 rounded-l-3xl p-2 align-middle duration-300 hover:bg-white/20 text-xl animate-bounce'>
+                    {/* <ThemeSwitch /> */}
+                </span>
 
-            {
-                footer ?
-                    <Footer />
-                    :
-                    null
-            }
+                {
+                    navbar ?
+                        <Navbar />
+                        :
+                        null
+                }
 
-        </AppWrapper>
+                <div className='px-5 md:px-32'>
 
-        <script src="//code.jivosite.com/widget/OLF8iQTisE" async />
+                    {children}
 
-        {/* GTranslator widget script */}
-        <script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer />
+                    <div className="gtranslate_wrapper"></div>
 
-        <script
-            id="gtranslate-settings"
-            dangerouslySetInnerHTML={{
-                __html: `
+                </div>
+
+                {
+                    footer ?
+                        <Footer />
+                        :
+                        null
+                }
+
+            </AppWrapper>
+
+            {/* <script src="//code.jivosite.com/widget/OLF8iQTisE" async /> */}
+
+            {/* GTranslator widget script */}
+            {/* <script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer /> */}
+
+            {/* <script
+                id="gtranslate-settings"
+                dangerouslySetInnerHTML={{
+                    __html: `
             window.gtranslateSettings = {
             "default_language": "en",
             "detect_browser_language": true,
@@ -56,11 +74,12 @@ const Layout:React.FC<GlobalTypes> = ({ navbar, children, footer}) => (
             "flag_style": "3d"
             };
         `,
-            }}
-        />
+                }}
+            /> */}
 
-    </main>
+        </main>
+    )
 
-)
+}
 
 export default Layout
