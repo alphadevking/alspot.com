6bf755a7b880892e47d4d0a1ee4b3d8b0a86d0d6
alphadevking <98643372+alphadevking@users.noreply.github.com>
2023-09-18 18:10:59 +0100

    Update Hello.tsx

--- src/site/components/Sections/Hello.tsx
+++ src/site/components/Sections/Hello.tsx
@@ -9,11 +9,11 @@ const Hello = () => {
 
             <Typewriter className="text-3xl" text="Hello_"/>
 
-            <div className="text-justify">
-                I am a proficient full-stack software engineer known for my ability to construct fully integrated websites from the ground up, ensuring exceptional user experiences. My educational background includes a Bachelor's degree from Ambrose Alli University, earned in 2023, which provided me with a solid foundation in computer science.
-                Presently an intern at SMART EARNERS TEAM, I work as a web developer. In this capacity, I employ technologies such as React, TypeScript, and Web3 to craft dynamic and interactive web applications. I have a knack for swift learning and continuously strive to keep up with the latest industry standards and best practices.
-                My skill set extends to scripting languages and multimedia web technologies, including proficiency in HTML 5.0, Bootstrap 5, CSS 3.0, Python, JavaScript, Git, TypeScript, and ReactJs. These abilities enable me to produce efficient and clean code that adheres to industry standards, delivering top-tier user experiences.
-                In addition to my frontend capabilities, I've also honed my skills in backend development using Laravel and Django. I excel in integrating these frameworks seamlessly with frontend technologies like React, Vite, and Next.js, ensuring a cohesive and efficient web development process.
+            <div className="text-justify grid gap-2">
+                <div>I am a proficient full-stack software engineer known for my ability to construct fully integrated websites from the ground up, ensuring exceptional user experiences. My educational background includes a Bachelor's degree from Ambrose Alli University, earned in 2023, which provided me with a solid foundation in computer science.</div>
+                <div>Presently an intern at SMART EARNERS TEAM, I work as a web developer. In this capacity, I employ technologies such as React, TypeScript, and Web3 to craft dynamic and interactive web applications. I possess a natural aptitude for acquiring new knowledge and continuously strive to keep up with the latest industry standards and best practices.</div>
+                <div>My skill set extends to scripting languages and multimedia web technologies, including proficiency in HTML 5.0, Bootstrap 5, CSS 3.0, Python, JavaScript, Git, TypeScript, and ReactJs. These abilities enable me to produce efficient and clean code that adheres to industry standards, delivering top-tier user experiences.</div>
+                <div>In addition to my frontend capabilities, I've also honed my skills in backend development using Laravel and Django. I excel in integrating these frameworks seamlessly with frontend technologies like React, Vite, and Next.js, ensuring a cohesive and efficient web development process.</div>
             </div>
 
             <a target='_blank' href={'/alphadevking.pdf'} className="flex gap-2 align-middle text-blue-500">
